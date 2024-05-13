38737f863586e7dfe91a44efe0177ba47f654453
siamsaleh <siamsaleh9@gmail.com>
2024-05-13 22:18:44 +0600

    Remove comments and unnecessary imports

--- app/src/main/java/com/schooling/telecevideoapp/activity/MainActivity.kt
+++ app/src/main/java/com/schooling/telecevideoapp/activity/MainActivity.kt
@@ -36,15 +36,16 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         setContentView(binding.root)
 
-        /*firebaseNotification()*/
-
-        /*loadLiveData()*/
         observeLiveData()
 
         // Video Table
         initializeDatabase()
 
+        // FCM Notification
+        fcmNotification()
+    }
 
+    private fun fcmNotification() {
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
                 Log.w("FCM_TAG", "Fetching FCM registration token failed", task.exception)
@@ -54,56 +55,10 @@ class MainActivity : AppCompatActivity() {
             // Get new FCM registration token
             val token = task.result
 
-            // Log and toast
-            /*val msg = getString(R.string.msg_token_fmt, token)*/
+            // See Token
             Log.d("FCM_TAG", token)
-            Toast.makeText(baseContext, "device reg token is ", Toast.LENGTH_SHORT).show()
         })
     }
-
-    /*private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
-        } else {
-            // TODO: Inform user that that your app will not show notifications.
-        }
-    }*/
-
-    /*private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }*/
-
-    /*private fun firebaseNotification() {
-        notificationPermission()
-        FirebaseMessaging.getInstance().subscribeToTopic("notification")
-    }*/
-
-    /*private fun notificationPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            val permissionsNotification = ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS)
-            if (permissionsNotification != PackageManager.PERMISSION_GRANTED){
-                val NOTIFICATION_PERMISSION = arrayOf(POST_NOTIFICATIONS)
-                ActivityCompat.requestPermissions(this, NOTIFICATION_PERMISSION, 100)
-            }
-        }
-    }*/
 
     private fun initializeDatabase() {
 
--- app/src/main/java/com/schooling/telecevideoapp/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/schooling/telecevideoapp/service/MyFirebaseMessagingService.java
@@ -9,9 +9,7 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
-
 import androidx.core.app.NotificationCompat;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.schooling.telecevideoapp.R;
